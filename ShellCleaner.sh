7b36c0d1b89b01f147bfa7f0f5d9b123876eb2d5
hilmikh <109839617+hilmikh@users.noreply.github.com>
2022-07-24 17:17:43 +0700

    Add files via upload

---
+++ ShellCleaner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+csvstack 2019-Oct-sample.csv 2019-Nov-sample.csv > csvstack.csv
+
+csvcut -c event_time,event_type,product_id,category_id,brand,price,category_code csvstack.csv > csvcut.csv
+
+csvgrep -c event_type -m purchase csvcut.csv > csvgrep.csv
+
+csvsql --query "SELECT event_time,event_type,product_id,category_id,brand,price,SUBSTR(category_code,1,INSTR(category_code,'.')-1) as category, SUBSTR(category_code, INSTR(category_code,'.')+1, LENGTH(category_code)) as product_name FROM csvgrep" csvgrep.csv > csvsql1.csv
+csvsql --query "SELECT event_time,event_type,product_id,category_id,brand,price,category, SUBSTR(product_name, INSTR(product_name,'.')+1, LENGTH(product_name)) as product_name FROM csvsql1" csvsql1.csv > csvfinal.csv
+
+rm csvstack.csv csvcut.csv csvgrep.csv csvsql1.csvS
